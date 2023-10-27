d6acc59199252aa04b419e3f4772d6f386111333
Weslley Silva <weslley.the@gmail.com>
2023-10-27 14:04:52 -0300

    feat & fix : Log Errors, Fix Register and Authentication

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,13 +1,15 @@
 const mongoose = require("mongoose");
-require('dotenv').config();
+require("dotenv").config();
 
+mongoose.Promise = Promise;
 module.exports = () => {
-    mongoose.connect(process.env.MONGODB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
+  mongoose
+    .connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     })
     .then(() => {
-        console.log('MongoDB connected...');
+      console.log("MongoDB connected...");
     })
-    .catch((err: { message: any; }) => console.log(err.message));
-}
+    .catch((err: { message: any }) => console.log(err.message));
+};
