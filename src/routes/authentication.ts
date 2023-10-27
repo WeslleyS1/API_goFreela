97312a0c0df64b578f1c9c3973015b2a85da3c4f
Weslley Silva <weslley.the@gmail.com>
2023-10-27 14:30:35 -0300

    feat & fix : Log Errors, Fix Register and Authentication

--- src/routes/authentication.ts
+++ src/routes/authentication.ts
@@ -1,7 +1,8 @@
-import express from 'express';
+import express from "express";
 
-import {register} from '../controllers/authController';
+import { login, register } from "../controllers/authController";
 
 export default (router: express.Router) => {
-    router.post('/auth/register', register);
+  router.post("/auth/register", register);
+  router.post("/auth/login", login);
 };
